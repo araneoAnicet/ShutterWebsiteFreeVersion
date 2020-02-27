7925568faebf50956b059bbd4a810adadeeb2aca
Nikita Solomientsev <araneoanicet@gmail.com>
2020-02-27 18:26:40 +0100

    create basic html page

--- flaskapp/site/static/js/base.js
+++ flaskapp/site/static/js/base.js
@@ -4,6 +4,27 @@ new Vue({
         '[[', ']]'
     ],
     data: {
-        message: 'Hello from vue!'
+        message: 'Hello from vue!',
+        is_spinning: true,
+        is_signed_in: false,
+        username: 'username'
+    },
+    methods: {
+        show_spinner() {
+            this.is_spinning = true
+        },
+
+        hide_spinner() {
+            this.is_spinning = false
+        },
+
+        set_signed_in() {
+            this.is_signed_in = true
+        },
+
+        set_signed_out() {
+            this.is_signed_in = false
+        }
     }
 });
+
